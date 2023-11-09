06ffcbf283ce006cb80890613f19b694bf794f0e
Alex H <alex.holder@bigfootds.com>
2023-11-09 20:46:50 +1100

    feat: cors setup

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ const express = require('express');
 // make a server instance 
 const app = express();
 
+const cors = require('cors');
+const corsOptions = {
+	origin: ["http://localhost:3000", "https://someDeployedWebsite.com"],
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 // Enables request.body to be raw JSON data 
 app.use(express.json());
 
